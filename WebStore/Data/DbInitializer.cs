cf9c2474447c0f32aa08b93e8ba8ecf19616b139
BezdelMA <BezdelMA@mail.ru>
2020-03-24 01:22:24 +0300

    HW_5

---
+++ WebStore/Data/DbInitializer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebStore.DAL.Context;
+
+namespace WebStore.Data
+{
+    public class DbInitializer
+    {
+        private readonly WebStoreContext _db;
+
+        public DbInitializer(WebStoreContext db) => _db = db;
+
+        public void Initialize() => InitializeAsync().Wait();
+
+        public async Task InitializeAsync()
+        {
+            var db = _db.Database;
+            await db.MigrateAsync().ConfigureAwait(false);
+
+            if (await _db.Products.AnyAsync()) return;
+
+            using (var transaction = await db.BeginTransactionAsync().ConfigureAwait(false))
+            {
+                await _db.Sections.AddRangeAsync(TestData.Sections).ConfigureAwait(false);
+
+                await db.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Sections] ON");
+                await _db.SaveChangesAsync().ConfigureAwait(false);
+                await db.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Sections] OFF");
+
+                await transaction.CommitAsync().ConfigureAwait(false);
+            }
+
+            using (var transaction = await db.BeginTransactionAsync().ConfigureAwait(false))
+            {
+                await _db.Brands.AddRangeAsync(TestData.Brands).ConfigureAwait(false);
+
+                await db.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Brands] ON");
+                await _db.SaveChangesAsync().ConfigureAwait(false);
+                await db.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Brands] OFF");
+
+                await transaction.CommitAsync().ConfigureAwait(false);
+            }
+
+            using (var transaction = await db.BeginTransactionAsync().ConfigureAwait(false))
+            {
+                await _db.Products.AddRangeAsync(TestData.Products).ConfigureAwait(false);
+
+                await db.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Products] ON");
+                await _db.SaveChangesAsync().ConfigureAwait(false);
+                await db.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Products] OFF");
+
+                await transaction.CommitAsync().ConfigureAwait(false);
+            }
+        }
+    }
+}
--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -1,15 +1,21 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebStore.Controllers;
+using WebStore.DAL.Context;
+using WebStore.Data;
+using WebStore.Infrastructure.Implementations.Sql;
 using WebStore.Infrastructure.Interfaces;
 using WebStore.Infrastructure.Services;
-using WebStore.ViewModels;
+
 
 namespace WebStore
 {
@@ -17,16 +23,27 @@ namespace WebStore
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<WebStoreContext>(opt =>
+                opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddTransient<DbInitializer>();
+
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
             services.AddSingleton<IEmployeesData, InMemoryEmployeesData>();
-            services.AddSingleton<IProductData, InMemoryProductData>();
+            services.AddScoped<IProductData, SqlProductData>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DbInitializer db)
         {
+            db.Initialize();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -38,6 +55,10 @@ namespace WebStore
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapGet("/greetings", async context =>
+                {
+                    await context.Response.WriteAsync(Configuration["CustomGreetings"]);
+                });
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{Id?}");
